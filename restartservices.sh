92e710134e38c360355ad2aac4aa9ed5820f2f74
Axton <priceroan2@gmail.com>
2022-02-18 04:02:18 +0000

    commit

--- restartservices.sh
+++ restartservices.sh
@@ -1,11 +1,15 @@
-echo "Attempting to restart Apache..."
+red=`tput setaf 1`
+green=`tput setaf 2`
+blue=`tput setaf 4`
+reset=`tput sgr0`
+echo "${blue}Attempting to restart Apache...${reset}"
 sudo systemctl restart apache2
-echo "✔️ Apache2 restarted!"
-echo "Attempting to restart Nginx..."
+echo "${green}✔️ Apache2 restarted!${reset}"
+echo "${blue}Attempting to restart Nginx...${reset}"
 sudo systemctl restart nginx
-echo "✔️ Nginx restarted!"
-echo "Attempting to restart Wings..."
+echo "${green}✔️ Nginx restarted!${reset}"
+echo "${blue}Attempting to restart Wings...${reset}"
 sudo systemctl restart wings
-echo "✔️ Wings restarted!"
+echo "${green}✔️ Wings restarted!${reset}"
 echo ""
-echo "---> The Pterodactyl services have been restarted!"
+echo "${green}---> The Pterodactyl services have been restarted!${reset}"
