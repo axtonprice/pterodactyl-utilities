0ea0209bef26478b2e78be5ed2b60bda6d6299e8
Axton <enquires@axtonprice.com>
2022-02-18 04:15:09 +0000

    commit

--- restartservices.sh
+++ restartservices.sh
@@ -1,14 +1,9 @@
-red=`tput setaf 1`
 green=`tput setaf 2`
-blue=`tput setaf 4`
 reset=`tput sgr0`
-echo "${blue}Attempting to restart Apache...${reset}"
 sudo systemctl restart apache2
 echo "${green}✔️ Apache2 restarted!${reset}"
-echo "${blue}Attempting to restart Nginx...${reset}"
 sudo systemctl restart nginx
 echo "${green}✔️ Nginx restarted!${reset}"
-echo "${blue}Attempting to restart Wings...${reset}"
 sudo systemctl restart wings
 echo "${green}✔️ Wings restarted!${reset}"
 echo ""
