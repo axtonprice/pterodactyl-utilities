6ed04c2b1f30c575419a18eb977aacec5e48645c
Axton <priceroan2@gmail.com>
2022-02-18 02:22:50 +0000

    commit

--- restartservices.sh
+++ restartservices.sh
@@ -1,3 +1,3 @@
 sudo systemctl restart nginx
 sudo systemctl restart wings
-echo -e "\e[32m---> The Pterodactyl services have been restarted!\e[0m"
+echo "---> The Pterodactyl services have been restarted!"
--- updatepanel.sh
+++ updatepanel.sh
@@ -1,3 +1,3 @@
 cd /var/www/pterodactyl && php artisan p:upgrade
 sudo systemctl restart wings
-echo -e "\e[32m---> The panel has sucessfully been updated!\e[0m"
+echo "---> The panel has sucessfully been updated!"
