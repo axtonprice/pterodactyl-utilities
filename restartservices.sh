9390669ee49120fcac01e84fd3917cc7ad4cab19
Axton <enquires@axtonprice.com>
2022-02-18 04:24:48 +0000

    Final Commit

--- restartservices.sh
+++ restartservices.sh
@@ -1,12 +1,7 @@
 # Axton P.#1234
-green=`tput setaf 2`
-reset=`tput sgr0`
 sudo systemctl restart apache2
-echo "${green}✔️ Apache2 restarted!${reset}"
 sudo systemctl restart nginx
-echo "${green}✔️ Nginx restarted!${reset}"
 sudo systemctl restart wings
-echo "${green}✔️ Wings restarted!${reset}"
 echo ""
-echo "---> The Pterodactyl services have been restarted!${reset}"
+echo "---> The Pterodactyl services have been restarted!"
 echo ""
