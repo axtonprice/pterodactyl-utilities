aa12f8ff52b9c5bea66e541df4ded59d65f650aa
Axton <priceroan2@gmail.com>
2022-02-18 02:24:12 +0000

    commit

--- restartservices.sh
+++ restartservices.sh
@@ -1,3 +1,6 @@
 sudo systemctl restart nginx
+sudo systemctl restart apache
+sudo systemctl restart apache2
 sudo systemctl restart wings
+clear
 echo "---> The Pterodactyl services have been restarted!"
