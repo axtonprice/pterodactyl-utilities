21e6ee0c8126f21f11b3d87e8c78f125f76d9974
Axton <enquires@axtonprice.com>
2022-02-18 04:07:59 +0000

    commit
    Co-Authored-By: blasterock <99603776+blasterock@users.noreply.github.com>

--- updatewings.sh
+++ updatewings.sh
@@ -2,4 +2,3 @@ sudo systemctl stop wings
 sudo curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 sudo chmod u+x /usr/local/bin/wings
 sudo systemctl restart wings
-# echo "---> Wings has successfully been upgraded!"
