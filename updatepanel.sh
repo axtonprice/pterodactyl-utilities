5d5e86692dcccdf9f85ab63a6e7dcde039da11a5
Axton <priceroan2@gmail.com>
2022-02-18 01:55:19 +0000

    commit

--- updatepanel.sh
+++ updatepanel.sh
@@ -1,4 +1,3 @@
 cd /var/www/pterodactyl && php artisan p:upgrade
 sudo systemctl restart wings
-clear
 echo "---> The panel has sucessfully been updated!"
--- updatewings.sh
+++ updatewings.sh
@@ -1,5 +1,4 @@
 sudo curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 sudo chmod u+x /usr/local/bin/wings
 sudo systemctl restart wings
-clear
 echo "---> Wings has successfully been updated!"
